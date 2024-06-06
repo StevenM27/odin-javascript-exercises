3c66ec1985e1d0fc71c482deb9247470f0b2ccfa
Steven Martin <83900183+StevenM27@users.noreply.github.com>
2024-06-05 22:33:34 -0700

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    answer = array;
+
+    for (let i = 0; i < remove.length; i++) {
+        answer = answer.filter( item => item !== remove[i]);
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
