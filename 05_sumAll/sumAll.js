03ffc7e365ca9f510fde387556b443dedc2cf669
Steven Martin <83900183+StevenM27@users.noreply.github.com>
2024-06-05 22:43:13 -0700

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    answer = 0;
 
+    if (typeof start !== 'number' || typeof end !== 'number' ||
+        end < 0 || start < 0) {
+        return 'ERROR';
+    }
+
+    if (end < start) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+
+    for (let i = start; i <= end; i++) {
+        answer += i;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
