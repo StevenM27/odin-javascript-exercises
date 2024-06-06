f7e52c784a05ea1cc30c31650e3c7da4fcfc3f40
Steven Martin <83900183+StevenM27@users.noreply.github.com>
2024-06-05 22:20:52 -0700

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  answer = '';
+  array = string.split('');
 
+  for (let i = 0; i < array.length; i++) {
+    answer += array[array.length - i - 1];
+  }
+
+  return answer;
 };
 
 // Do not edit below this line
