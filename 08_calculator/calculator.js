6f2bf9e84194e4cb95b3592d48b92a59bdbb3d4f
Steven Martin <83900183+StevenM27@users.noreply.github.com>
2024-06-13 18:51:40 -0700

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, number) => product * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, e) {
+  return a ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	total = 1;
+
+  while (number > 1) {
+    total *= number;
+    number--;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
